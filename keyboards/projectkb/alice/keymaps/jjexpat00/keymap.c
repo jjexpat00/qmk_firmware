e04ac44e4cbb2e33290248223eed39c6ab83b0f3
Jacob Chow <jacobchow@pinger.com>
2020-01-29 15:47:04 -0800

    Modify pkb wonderland alice keymap

--- keyboards/projectkb/alice/keymaps/jjexpat00/keymap.c
+++ keyboards/projectkb/alice/keymaps/jjexpat00/keymap.c
@@ -23,20 +23,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // entirely and just use numbers.
 #define _BASE 0
 #define _FN1 1
+#define _FN2 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_default(
-    KC_ESC,  KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
+    KC_ESC,  KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
     KC_PGUP, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_PGDN, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RGUI,
-             KC_LCTL,          KC_LALT, KC_SPC,  MO(_FN1),                          KC_SPC,           KC_RALT,          KC_RCTL
+    KC_PGDN, MO(_FN1),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN2),
+             KC_LCTL,          KC_LALT, KC_LGUI,  KC_SPC,                           KC_SPC,           KC_RALT,          KC_RCTL
   ),
 
   [_FN1] = LAYOUT_default(
     RGB_TOG,  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_DEL,
-    RGB_MOD,  _______,  _______, KC_UP,  _______, _______, _______,          RGB_SAI, RGB_HUI, RGB_VAI, _______, _______, _______, _______, _______,
-    RGB_RMOD, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          RGB_SAD, RGB_HUD, RGB_VAD, _______, _______, _______,          _______,
+    KC_HOME,  _______, LCTL(LSFT(KC_TAB)),LGUI(KC_W),LCTL(KC_TAB),_______,_______,          _______,_______,LCTL(KC_A),_______,_______,_______,_______,_______,
+    KC_END,   _______, _______, _______, _______, _______, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, LCTL(KC_E),   _______,          _______,
+              _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+              _______,          _______, _______, _______,                            _______,          _______,          RESET
+  ),
+
+  [_FN2] = LAYOUT_default(
+    RGB_TOG,  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_DEL,
+    RGB_MOD,  _______, RGB_SAI, RGB_HUI, RGB_VAI, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
+    RGB_RMOD, _______, RGB_SAD, RGB_HUD, RGB_VAD, _______, _______,          _______, _______, _______, _______, _______, _______,          _______,
               _______, BL_INC,  BL_DEC,  BL_TOGG, BL_BRTG, _______,          _______, _______, _______, _______, _______, _______, _______, _______,
               _______,          _______, _______, _______,                            _______,          _______,          RESET
   )
